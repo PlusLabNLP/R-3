896cc1bf95146f9ab3e7b22affec219b4211d6e9
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2020-04-02 03:34:50 -0700

    Update utils.py

--- comet-commonsense/utils/utils.py
+++ comet-commonsense/utils/utils.py
@@ -195,7 +195,7 @@ def filter_beam_output(arr, inp):
     for i in range(0, 5):
         beam_w = arr[i].split()
         tags = nltk.pos_tag(nltk.word_tokenize(arr[i]))
-        if len(set(beam_w).intersection(set(words))) > 0 or ('allerg' in arr[i] and 'allerg' in inp ):
+        if (len(set(beam_w).intersection(set(words))) > 0 and ('feel' not in beam_w)) or ('allerg' in arr[i] and 'allerg' in inp ):
             continue
         if len(beam_w)==1:
             try:
